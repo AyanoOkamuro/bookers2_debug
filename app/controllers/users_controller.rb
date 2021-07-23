b0b2679420e256feca7e5b7004a247aa37791b54
AyanoOkamuro <omochi185@gmail.com>
2021-07-23 09:15:37 +0000

    Update 16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,9 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to users_path, notice: "You have updated user successfully."
     else
-      render "show"
+      render :edit
     end
   end
 
@@ -35,5 +35,5 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user)
     end
   end
-  
+
 end
